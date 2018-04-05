94d0af1f283efcb0ab47f8a53e8eb5424ce2405c
Adnan Usman <adnantu@hotmail.com>
2018-04-04 22:17:07 -0700

    removed unnecessary console.log

--- frontend-nanodegree-arcade-game/sw.js
+++ frontend-nanodegree-arcade-game/sw.js
@@ -32,7 +32,6 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
